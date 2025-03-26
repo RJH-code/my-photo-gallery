a16a5b74e5199fb45e7b9c990c4f89707f934472
Ruudi <the.lune@hotmail.com>
2025-03-26 12:16:17 +0200

    Generated/updated manifest.txt files

---
+++ images/generate-manifests.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd /d "%~dp0images"
+
+for /d %%Y in (*) do (
+  for /d %%C in ("%%Y\*") do (
+    > "%%C\manifest.txt" (
+      for %%I in ("%%C\*.jpg" "%%C\*.jpeg" "%%C\*.png" "%%C\*.webp") do (
+        echo %%~nxI
+      )
+    )
+    echo Created manifest.txt in %%C
+  )
+)
+
+echo All manifests updated!
+pause
