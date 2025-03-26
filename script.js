90710776c613293588be50221e8f5fcb9c42b4b4
Ruudi <the.lune@hotmail.com>
2025-03-26 10:15:36 +0200

    Updated script.js to use custom image filenames

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const gallery = document.getElementById("gallery");
 const yearLinks = document.querySelectorAll("#year-list a");
 
+const imagesByYear = {
+  "2023": ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"],
+  "2022": ["2022.jpg"]
+};
+
 const loadImages = (year) => {
   gallery.innerHTML = "";
+  const files = imagesByYear[year] || [];
 
-  for (let i = 1; i <= 5; i++) {
+  files.forEach(file => {
     const img = document.createElement("img");
-    img.src = `images/${year}/${i}.jpg`;
-    img.alt = `${year} image ${i}`;
+    img.src = `images/${year}/${file}`;
+    img.alt = `${year} image`;
     gallery.appendChild(img);
-  }
+  });
 };
 
 yearLinks.forEach(link => {
